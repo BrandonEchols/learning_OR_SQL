bf1babe89e6cffa47d69b7e3546bbc9f6a11f70f
Brandon Echols <bechols@digis.net>
2017-05-01 12:59:28 -0600

    Update Queries.sql
    Added three (and a half) sql queries.

--- queries.sql
+++ queries.sql
@@ -17,3 +17,15 @@ SELECT
   TREAT(deref(p.BOUGHTBY) AS DEPARTMENT_TYPE).deptID AS department,
   d.*
 FROM purchases p, TABLE (p.DEVICES_BOUGHT) d;
+
+--Look up a user based on the card that they scanned in which gives the userId
+SELECT * FROM USERS WHERE USERID = 1006;
+
+--Update the address for a location in the system
+UPDATE LOCATIONS SET ADDRESS = ADDRESS_TYPE('1234 Paradise Way', 'Orem', 'UT', '84604') WHERE LOCID = '400';
+
+--Find out if the device sitting in front of you is already in the system
+SELECT * FROM DEVICES WHERE SERIALNUM = 'DDD23SSSE203';
+
+--Get a list of all Laptops in the system
+SELECT * FROM DEVICES WHERE type
